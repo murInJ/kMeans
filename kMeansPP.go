d367c8eda2a42986a9e9153bb75d0a6af023690a
murinj <2248741593@qq.com>
2023-04-03 15:50:51 +0800

    v0.0.3

--- kMeansPP.go
+++ kMeansPP.go
@@ -20,12 +20,12 @@ func (b Vector) euclideanDistance(a Vector) float64 {
 	return math.Sqrt(sum)
 }
 
-type cluster struct {
+type Cluster struct {
 	Center  Vector
 	Vectors []Vector
 }
 
-func (c *cluster) updateCenter() {
+func (c *Cluster) updateCenter() {
 	center := make([]float64, len(c.Vectors[0]))
 	n := float64(len(c.Vectors))
 
@@ -42,16 +42,31 @@ func (c *cluster) updateCenter() {
 	c.Center = center
 }
 
-func KMeansPP(points []Vector, k int, iter int) []cluster {
+type KMeans struct {
+	points   []Vector
+	clusters []Cluster
+	k        int
+}
+
+func NewKmeans(points []Vector, k int) *KMeans {
+	kmeans := &KMeans{
+		points:   points,
+		k:        k,
+		clusters: make([]Cluster, k),
+	}
+	kmeans.initCluster()
+	return kmeans
+}
+func (kmeans *KMeans) initCluster() {
 	// Step 1: Choose the first center uniformly at random
-	centers := make([]Vector, k)
-	centers[0] = points[rand.Intn(len(points))]
-	dists := make([]float64, len(points))
+	centers := make([]Vector, kmeans.k)
+	centers[0] = kmeans.points[rand.Intn(len(kmeans.points))]
+	dists := make([]float64, len(kmeans.points))
 
 	// Step 2: Choose the remaining centers via weighted sampling
-	for i := 1; i < k; i++ {
+	for i := 1; i < kmeans.k; i++ {
 		sumDist := 0.0
-		for j, p := range points {
+		for j, p := range kmeans.points {
 			minDist := math.MaxFloat64
 			for _, c := range centers[:i] {
 				dist := p.euclideanDistance(c)
@@ -64,17 +79,20 @@ func KMeansPP(points []Vector, k int, iter int) []cluster {
 		}
 		randVal := rand.Float64() * sumDist
 		index := 0
-		for ; index < len(points)-1 && randVal > 0; index++ {
+		for ; index < len(kmeans.points)-1 && randVal > 0; index++ {
 			randVal -= dists[index]
 		}
-		centers[i] = points[index]
+		centers[i] = kmeans.points[index]
 	}
 
 	// Step 3: Initialize the clusters
-	clusters := make([]cluster, k)
-	for i := range clusters {
-		clusters[i].Center = centers[i]
+
+	for i := range kmeans.clusters {
+		kmeans.clusters[i].Center = centers[i]
 	}
+}
+
+func (kmeans *KMeans) Train(iter int) []Cluster {
 
 	// Step 4: Run the iterative algorithm
 	cnt := 0
@@ -84,26 +102,26 @@ func KMeansPP(points []Vector, k int, iter int) []cluster {
 		}
 		cnt++
 
-		// Update the cluster assignments
-		for i := range clusters {
-			clusters[i].Vectors = nil
+		// Update the Cluster assignments
+		for i := range kmeans.clusters {
+			kmeans.clusters[i].Vectors = nil
 		}
-		for _, p := range points {
+		for _, p := range kmeans.points {
 			minDist := math.MaxFloat64
 			index := 0
-			for j, c := range clusters {
+			for j, c := range kmeans.clusters {
 				dist := p.euclideanDistance(c.Center)
 				if dist < minDist {
 					minDist = dist
 					index = j
 				}
 			}
-			clusters[index].Vectors = append(clusters[index].Vectors, p)
+			kmeans.clusters[index].Vectors = append(kmeans.clusters[index].Vectors, p)
 		}
 
 		// Exit condition: no point changes clusters
 		changed := false
-		for _, c := range clusters {
+		for _, c := range kmeans.clusters {
 			oldCenter := c.Center
 			c.updateCenter()
 			if c.Center.euclideanDistance(oldCenter) > 1e-6 {
@@ -115,5 +133,9 @@ func KMeansPP(points []Vector, k int, iter int) []cluster {
 		}
 	}
 
-	return clusters
+	return kmeans.clusters
+}
+
+func (kmeans KMeans) AddPoints(points []Vector) {
+	kmeans.points = append(kmeans.points, points...)
 }
